bdaade52300fadcafd690b8213da019fd94731f9
JoakBuoy <b.joak@alustudent.com>
2022-10-27 14:06:50 -0700

    :robot: automated commit of one or many files

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+
+def main(argv):
+    args = argv[1:]
+    args_len = len(args)
+    if args_len != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+        if args[1] != '+' and args[1] != '-' and args[1] != '*' and args[1] != '/':
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+            a = int(args[0])
+            b = int(args[2])
+            operator = args[1]
+            if operator == '+':
+                result = add(a, b)
+            elif operator == '-':
+                result = sub(a, b)
+            elif operator == '*':
+                result = mul(a, b)
+            elif operator == '/':
+                result = div(a, b)
+                print("{} {} {} = {}".format(a, operator, b, result))
+
+
+                if __name__ == "__main__":
+                    import sys
+                    main(sys.argv)
---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def main():
+    for name in dir(hidden_4):
+        if name.startswith('__'):
+            continue
+        print(name)
+
+
+        if __name__ == '__main__':
+            main()
