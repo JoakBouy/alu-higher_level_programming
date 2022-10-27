7b9a95b55ca444dc455c8c6e6d360f69b1419a53
JoakBuoy <b.joak@alustudent.com>
2022-10-27 04:00:13 -0700

    Fix checker fail

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+last_digit = number % 10
 
-last_digit = abs(number) % 10
 if number < 0:
-    last_digit = -last_digit
-    print(f"Last digit of {number:d} is {last_digit:d} and is ", end="")
-if last_digit > 5:
-    print("greater than 5")
-elif last_digit == 0:
-    print("0")
-else:
-    print("less than 6 and not 0")
+        last_digit = ((-1 * number) % 10) * -1
+
+if int(last_digit) > 5:
+    print("Last digit of {:d} is {:d}".format(number, last_digit), end=" ")
+    print("and is greater than 5")
+
+elif int(last_digit) == 0:
+    print("Last digit of {:d} is {:d}".format(number, last_digit), end=" ")
+    print("and is 0")
+
+elif int(last_digit) < 6:
+    print("Last digit of {:d} is {:d}".format(number, last_digit), end=" ")
+    print("and is less than 6 and not 0")
