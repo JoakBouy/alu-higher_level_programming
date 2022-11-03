50c0890ce5de29b087e8ddcc7655ebc6ddca67c8
JoakBuoy <b.joak@alustudent.com>
2022-11-03 16:02:01 -0700

    :robot: automated commit of one or many files

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    copy_list = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            copy_list.append(replace)
-            continue
-        copy_list.append(my_list[i])
-    return copy_list
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
+    return [find if find != search else replace for find in my_list]
