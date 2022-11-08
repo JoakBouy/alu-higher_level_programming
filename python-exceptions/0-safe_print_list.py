6ad8c9b2264ca08cb4999f0e2f42db3e815b5bd3
Buoy <bouygai3@gmail.com>
2022-11-08 04:28:09 +0300

    All tasks completed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     i = 0
-    for i in range(x):
-        try:
-            print('{}'.format(my_list[i]), end='')
+    try:
+        while i is not x:
+            print(my_list[i], end='')
             i += 1
-        except IndexError:
-            break
-        print()
-        return i
-
+    except IndexError:
+        None
+    print()
+    return i
