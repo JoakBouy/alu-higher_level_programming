a0d1737d80c55653058ea0c05e32d2748aa99214
Buoy <bouygai3@gmail.com>
2022-11-10 02:10:50 +0300

    Pep8 fix

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 """Define classes for a singly-linked list."""
 class Node:
     """Represent a node in a singly-linked list."""
--- python-classes/103-magic_class.py
+++ python-classes/103-magic_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
+
 """Define a MagicClass matching exactly a bytecode provided by Holberton."""
+
 
 import math
 class MagicClass:
