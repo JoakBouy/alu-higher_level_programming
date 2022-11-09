483e90454f06ab43e508d3af8c3c1c374d07c8b6
JoakBuoy <b.joak@alustudent.com>
2022-11-09 00:40:13 -0800

    :robot: automated commit of one or many files

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
-    i + 0
-    for index in range(x):
+    count = 0
+    num = 0
+    while count < x:
         try:
-            print("{:d}".format(my_list[index]), end="")
-            i += 1
-        except (ValueError, TypeError):
+            print("{:d}".format(my_list[count]), end="")
+            num += 1
+        except (TypeError, ValueError):
             pass
-        print()
-        return i
+        count += 1
+    print()
+    return num
