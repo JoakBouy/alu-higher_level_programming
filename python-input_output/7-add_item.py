207925799f03c717956d52d9feae2fffa9b1882f
Buoy <b.joak@alustudent.com>
2022-11-17 12:15:57 +0300

    Checher fail fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""add all arguments to a Python list, and then save them to a file."""
+"""Add all arguments to a Python list, and then save them to a file"""
 
 
 import sys
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
-all_list = [arg for arg in sys.argv[1:]]
-try:
-    items = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    items = []
-items.extend(all_list)
-save_to_json_file(items, "add_item.json")
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file') \
+        .load_from_json_file
+
+    try:
+        loadfile = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        loadfile = []
+
+    arg = len(sys.argv)
+    for i in range(1, arg):
+        loadfile.append(sys.argv[i])
+    save_to_json_file(loadfile, "add_item.json")
