99a258a1e1df63ec0dec44da06c396c14e9cdc93
JoakBouy <b.joak@alustudent.com>
2022-11-26 08:01:25 +0000

    Script that fetches alu intranet status

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+   request = urlib.request.Request("https://intranet.htbn.io/status")
+   with urlib.request.urlopen(request) as response:
+	body = response.read()
+	print("Body response")
+	print("\t- type: {}".format(type(body)))
+	print("\t- content: {}".format(body))
+	print("\t- utf8 content: {}".format(body.decode("utf-8")))
