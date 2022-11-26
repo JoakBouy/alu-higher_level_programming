42ee75fd4f0d92a27971fd047b9fd0f2e6378dd7
JoakBouy <b.joak@alustudent.com>
2022-11-26 08:27:46 +0000

    Task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,6 +4,10 @@ import urllib.request
 import sys
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as res:
-        print(res.info()['X-Request-Id'])
+if __name__ = "__main__":
+	import urllib.request
+	import sysy
+
+	with urllib.request.urlopen(sys.argv[1] as response:
+		head = response.headers.get('X-Request-Id')
+		print(head)
