6a1c3b39157208d6f09b186f05f383d6bf2934a8
JoakBuoy <b.joak@alustudent.com>
2022-11-30 21:09:43 +0300

    Functions that prints myname

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function that prints My name is <first name> <last name>"""
+
+
+def say_my_name(first_name, last_name=""):
+    """function"""
+    try:
+        name = "My name is"
+        if type(first_name) is not str:
+            raise TypeError('first_name must be a string')
+        if type(last_name) is not str:
+            raise TypeError('last_name must be a string')
+        print("{} {} {}".format(name, first_name, last_name))
+    except:
+        raise
