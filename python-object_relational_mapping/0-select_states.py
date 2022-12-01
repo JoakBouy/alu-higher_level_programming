3ce6c3583b31608bc4dc21fe40b50db10e4af488
JoakBuoy <b.joak@alustudent.com>
2022-12-01 15:45:04 +0300

    Lists all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!?usr/bin/python3
+'''Lists all states from the database htbn_0e_0_usa'''
+
+
+if __name__ == '__main__':
+    from sys import argv 
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1],
+                        passwd=argv[2],
+                        db=argv[3]),
+    cr = db.cursor()
+    cr.execute("SELECT * from states ORDER BY states.id")
+    states = cr.fetchall()
+    for state in states:
+        print(state)
+    cr.close()
+    db.close()
